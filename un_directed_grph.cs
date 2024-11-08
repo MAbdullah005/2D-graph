5629de4166e8a4105e4d649fc422f62cecd177a6
Abdullah Ali <abdullahaliofc@gmail.com>
2024-11-08 10:51:44 -0800

    Update un_directed_grph.cs

--- un_directed_grph.cs
+++ un_directed_grph.cs
@@ -5,6 +5,22 @@ using System.Security.Cryptography;
 using System.Threading.Channels;
 namespace weightGraph_dsa
 {
+	 public class Path
+ {
+     private List<string> node=new List<string>();
+     public void addnode(string node)
+     {
+         this.node.Add(node);
+     }
+     public List<string> getpath()
+     {
+         return this.node;
+     }
+     public override string ToString()
+     {
+         return "node "+node.ToString()!; 
+     }
+ }
     public class Weight_Graph
     {
         private class Node
@@ -70,49 +86,79 @@ namespace weightGraph_dsa
                 Console.WriteLine();
             }
         }
-	        {
-       Dictionary<Node,Node> previos = new Dictionary<Node,Node>();
-       Dictionary<Node,int> distance= new Dictionary<Node,int>();
-        HashSet<Node> set = new HashSet<Node>();
-       PriorityQueue<Node,int> quee=new PriorityQueue<Node,int>();
-        quee.Enqueue(_nodes[data],0);
-        while(quee.Count > 0)
+	    
+  public Path shrotPath(String from, String to)
+     {
+        Dictionary<Node, int> distance = new Dictionary<Node, int>();
+        Dictionary<Node, Node> presiosnode = new Dictionary<Node, Node>();
+        foreach (var node in _nodes.Values)
         {
-            Node node = quee.Dequeue();
-            foreach(var item in  node.getedges())
+            distance.Add(node, int.MaxValue);
+        }
+        var tonode = _nodes[to];
+        var tofrom = _nodes[from];
+        distance[_nodes[from]] = 0;
+        HashSet<Node> visited = new HashSet<Node>();
+        PriorityQueue<Node, int> quee = new PriorityQueue<Node, int>();
+        quee.Enqueue(_nodes[from], 0);
+        while (quee.Count > 0)
+        {
+            var current = quee.Dequeue();
+            visited.Add(current);
+            foreach (var edge in current.getedges())
             {
-               if(quee.UnorderedItems.Any(i=>i.Element==node))
+                if (visited.Contains(edge.to))
+                { continue; }
+                int newdis = distance[current] + edge.wight;
+                if (newdis < distance[edge.to])
                 {
-
-                    
+                    distance[edge.to] = newdis;
+                    quee.Enqueue(edge.to, newdis);
+                    presiosnode[edge.to] = current;
+                }
             }
         }
+        return buildPath(presiosnode, tonode);
     }
-   private bool balance(Dictionary<Node, Node> previos,Dictionary<Node, int> distance,Node from,Node to,int wight)
+    private Path buildPath(Dictionary<Node, Node> presiosnode, Node tonode)
     {
-         if(!distance.ContainsKey(from))
+        Stack<Node> stack = new Stack<Node>();
+        stack.Push(tonode);
+        var previos = presiosnode[tonode];
+        while (previos != null)
         {
-            distance.Add(from,wight);
-            previos.Add(to,from);
-        }   
-         else if (distance[from]>wight)
-        {
-            distance[from] = wight;
+            stack.Push(previos);
+            previos = presiosnode.GetValueOrDefault(previos);
         }
+        Path path = new Path();
+        while (stack.Count > 0)
+        {
+            path.addnode(stack.Pop().lable);
+        }
+        return path;
     }
 }
-    }
+}
         public class Progarm
         {
             static void Main()
             {
-               Weight_Graph weight_Graph = new Weight_Graph();
-            weight_Graph.addnode("A");
-            weight_Graph.addnode("B");
-            weight_Graph.addnode("C");
-            weight_Graph.addedge("A","B",3);
-            weight_Graph.addedge("A","C",2);
-            weight_Graph.print();
+              Weight_Graph weight_Graph = new Weight_Graph();
+              weight_Graph.addnode("A");
+              weight_Graph.addnode("B");
+              weight_Graph.addnode("C");
+              weight_Graph.addnode("D");
+              weight_Graph.addnode("E");
+              weight_Graph.addnode("L");
+              weight_Graph.addedge("A", "B", 3);
+              weight_Graph.addedge("A", "C", 4);
+              weight_Graph.addedge("A", "D", 2);
+              weight_Graph.addedge("B", "D", 6);
+              weight_Graph.addedge("B", "E", 1);
+              weight_Graph.addedge("D", "E", 5);
+              weight_Graph.addedge("D", "C", 1);
+              weight_Graph.addedge("E", "L", 4);
+              weight_Graph.shrotPath("A", "E");
             }
         }
 }
